6faba44c9e96cb9f331d0a534f77c1965a0eb183
soottoppi <duwjs1103@naver.com>
2021-11-15 12:29:50 +0900

    first commit

---
+++ project-ex02/app-ex01.js
@@ -0,0 +1,9 @@
+/*
+douzone-soottoppi-math 파일 모듈 테스트
+*/
+
+var dzMath = require("../douzone-soottoppi-math/douzone-soottoppi-math");
+
+console.log(dzMath.sum(10, 20, 30, 40, 50));
+console.log(dzMath.max(10, 20, 30, 40, 50));
+console.log(dzMath.min(10, 20, 30, 40, 50));
